cacef583ae1f7253522cef26f8c5b6f42298a463
Hasan YAŞAR <hasanyasar07@hotmail.com>
2023-08-18 15:14:53 +0300

    Version1

--- src/Hello_Git.java
+++ src/Hello_Git.java
@@ -2,6 +2,7 @@ public class Hello_Git {
     public static void main(String[] args) {
         System.out.println("Hello Github");
         System.out.println("Ben geldim..");
+        System.out.println("Değişikleri Kaydet...");
 
     }
 }
